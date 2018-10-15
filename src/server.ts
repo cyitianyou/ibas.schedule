b6e024030b2726634407238e08de8b519c53d544
cyitianyou <cyitianyou@gmail.com>
2018-10-15 17:42:40 +0800

    webservice启动定时器,堵塞线程

--- src/server.ts
+++ src/server.ts
@@ -11,13 +11,3 @@ const server: Server = createServer(currentApp);
 server.listen(config.appSettings.port || 3000, () => {
     console.log("starting at port " + config.appSettings.port);
 });
-// 热加载
-if (module.hot) {
-    // 监听./app.ts
-    module.hot.accept("./app.ts", () => {
-        // 如果有改动，就使用新的app来处理请求
-        server.removeListener("request", currentApp);
-        currentApp = app.callback();
-        server.on("request", currentApp);
-    });
-}
--- src/service/DataService.ts
+++ src/service/DataService.ts
@@ -6,6 +6,8 @@
  * that can be found in the LICENSE file at http://www.apache.org/licenses/LICENSE-2.0
  */
 /// <reference path="../index.d.ts" />
+import { Schedule } from "../schedule/Sehedule";
+import Loader from "../loader/index";
 import { OperationResult, OperationInformation, OperationMessage } from "./OperationResult";
 import { DataConverter4js, IDataConverter } from "./DataConverter";
 import Service, { IMethodCaller } from "./Service";
@@ -13,20 +15,30 @@ export default class DataService extends Service {
     protected get converter(): IDataConverter {
         return new DataConverter4js();
     }
-    public showSeheduleStatus(caller: IMethodCaller<string>): void {
+    public startSehedule(caller: IMethodCaller<string>): void {
         let opRslt: OperationResult<string> = new OperationResult<string>();
         try {
             let url: string = caller.query.url;
-            if (!global.window
-                || global.window.document.URL !== url) {
-                opRslt.addResults("UN");
-            }
-            caller.onComplete(opRslt);
+            let config: Config = require("../config");
+            let loader: Loader = new Loader();
+            loader.noCache = true;
+            loader.minLibrary = true;
+            loader.user = config.appSettings.defaultUser;
+            loader.password = config.appSettings.defaultPassword;
+            process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+            let stratTime: Date = new Date();
+            loader.run(url, function (): void {
+                let endTime: Date = new Date();
+                console.log(ibas.strings.format("加载vstore花费时间{0}秒",
+                    ibas.dates.difference(ibas.dates.emDifferenceType.SECOND, endTime, stratTime)));
+                let schedule: Schedule = new Schedule();
+                schedule.start();
+            });
         } catch (e) {
             opRslt.resultCode = 400;
             opRslt.message = e.message;
         }
-
+        caller.onComplete(opRslt);
     }
 
 }
