154b982e1b9e97d32ad7b5d82e3d17c785f09619
cyitianyou <cyitianyou@gmail.com>
2018-10-18 10:09:36 +0800

    服务端支持跨域

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,14 @@
 import * as Koa from "koa";
 import * as bodyParser from "koa-bodyparser";
 import * as logger from "koa-logger";
+import * as cors from "koa2-cors";
 import router from "./router";
 import log4js from "./logger/index";
 log4js.getLogger();
 let app: Koa = new Koa();
-app.use(logger());
-app.use(bodyParser());
+app.use(cors()); // 作用：跨域
+app.use(logger()); // 作用：日志
+app.use(bodyParser()); // 作用：处理post消息实体
 app.use(router.routes()); // 作用：启动路由
 app.use(router.allowedMethods()); // 作用： 当请求出错时的处理逻辑
 export default app;
